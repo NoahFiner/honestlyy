a1ce1b7715086655874026f01710750007c4c693
Noah Finer <nfiner0149@gmail.com>
2016-08-03 15:59:05 -0600

    Add cookies and make the game playable

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,12 +1,50 @@
 class QuestionsController < ApplicationController
   def show
-    @id = params[:id];
-    @question = Question.find(params[:id]);
+    @question = Question.find(params[:id])
     @choices = [@question.choice_a, @question.choice_b, @question.choice_c, @question.choice_d]
     render :layout => 'question_layout'
   end
 
+  def edit
+    @question = Question.find(params[:id])
+    case params[:choice]
+    when 'a'
+      @question.count_a += 1
+    when 'b'
+      @question.count_b += 1
+    when 'c'
+      @question.count_c += 1
+    when 'd'
+      @question.count_d += 1
+    end
+    @question.save
+    
+    puts "count of question #{params[:id]}"
+    puts [@question.count_a, @question.count_b, @question.count_c, @question.count_d]
+    redirect_to random_path
+  end
+
   def random
-    redirect_to questions_show_path(id: rand(Question.count)+1)
+    # cookies.delete :visited_questions
+    question_count = Question.count
+    rand_question = rand(question_count) + 1
+    if !(cookies[:visited_questions].nil?) && cookies[:visited_questions].split("-").length < question_count
+      visited_questions = cookies[:visited_questions].split("-")
+
+      while visited_questions.include? rand_question.to_s
+        rand_question = rand(question_count) + 1
+      end
+
+      visited_questions.push(rand_question)
+
+      cookies[:visited_questions] = {
+        value: visited_questions.join("-"),
+        expires: 1.day.from_now }
+
+    else
+      cookies[:visited_questions] = rand_question.to_s
+    end
+    puts "redirecting to #{rand_question}"
+    redirect_to questions_show_path(id: rand_question)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
 
   get '/questions/:id', to: 'questions#show'
+  get '/questions/:id/edit', to: 'questions#edit', as: 'choose'
   get '/random', to: 'questions#random'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
